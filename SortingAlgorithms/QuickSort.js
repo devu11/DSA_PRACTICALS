4795143658f3b87803aad39a841d31f72c86fd64
devu11 <devudevzz3@gmail.com>
2024-06-10 16:29:58 +0530

    added selection sort

---
+++ SortingAlgorithms/QuickSort.js
@@ -0,0 +1,19 @@
+function quicksort(array){
+    if(array.length<2){
+        return array
+    }
+    let pivot=array[array.length-1]
+    const left=[]
+    const right=[]
+    for(let i=0;i<array.length-1;i++){
+        if(array[i]<pivot){
+            left.push(array[i])
+        }else{
+            right.push(array[i])
+        }
+    }
+    return[...quicksort(left),pivot,...quicksort(right)]
+}
+
+const array=[3,1,9,0,2,10,29,33,21]
+console.log(quicksort(array))
